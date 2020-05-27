edabbf7d2df8c448107cbb0b14638fd70cac09da
John Kester <nnamanikester@gmail.com>
2020-05-27 01:31:03 +0100

    Changed API URL on Search.js & Img URL on markup searchviews.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,11 +9,17 @@ export default class Search {
 
   async searchByName() {
     try {
-      const res = await axios(`https://restcountries.eu/rest/v2/name/${this.query}?fullText=true`);
+      const res = await axios(`https://restcountries-v1.p.rapidapi.com/name/${this.query}`, {
+        headers: {
+          "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+          "x-rapidapi-key": "8b2040f64bmsh170259eb8d262f2p174604jsne4daa4f73d42",
+          "useQueryString": true
+        }
+      });
       this.result = res.data;
-      //console.log(this.result);
+      console.log(this.result);
     } catch (err) {
-      console.log(err);
+      console.log(err.response);
     }
   }
 }
--- src/js/views/searchviews.js
+++ src/js/views/searchviews.js
@@ -15,16 +15,16 @@ const renderCountry = (country) => {
   <div class="card">
     <div class="card__img">
       <a href="pages/details.html">
-        <img src="${country.flag}" alt="${country.name}">
-      </a>
-    </div>
-    <div class="card__details">
-      <h2 class="card__title">${country.name}</h2>
-      <span class="card__info">Population: ${country.population}</span>
-      <span class="card__info">Region: ${country.region}</span>
-      <span class="card__info">Capital: ${country.capital}</span>
-    </div>
+        <img src="https://assets.thebasetrip.com/api/v2/countries/flags/${country.name.replace(/\s/g, '-').toLowerCase()}.svg" alt="${country.name}">
+      </a >
+    </div >
+  <div class="card__details">
+    <h2 class="card__title">${country.name}</h2>
+    <span class="card__info">Population: ${country.population}</span>
+    <span class="card__info">Region: ${country.region}</span>
+    <span class="card__info">Capital: ${country.capital}</span>
   </div>
+  </div >
   `;
 
   domElements.cardContainer.insertAdjacentHTML("beforeend", markup);
